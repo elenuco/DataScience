2cb3c7bee7d487dea1b65525c7128a6f22457c7d
Evelyn Campos <elenuco36@gmail.com>
2021-08-16 00:08:32 -0600

    ejerciciod de tipos de valores y condicionales

--- Ejemplos1.py
+++ Ejemplos1.py
@@ -1,6 +1,43 @@
 x=1
-y=2
+print(type(x))
+y=4
+z=x+y 
+#suma
+print("Suma", z)
+#Resta
+z=x-y
+print("Resta", z)
+#Multiplicación
+z=x*y
+print("Multiplicación", z)
+#Division
+z=y/x 
+print("Division", z)
+#potencia
+z= y**x
+print("potencia", z)
+#División de piso
+z= y //x 
+print("Division de piso", z)
 if z>2:
 	print("five is greather than two")
 nombre=input("Escriba su nombre")
 print("Bienvenido" + nombre)
+print("hola mundo")
+a=str(3)
+b=int(3)
+z=float(3)
+print(a)
+print(b)
+print(z)
+s=33
+d=200
+if d>s:
+	print(type(b>a))
+	print((d>s))
+	print("d is greather than s")
+elif d==s:
+	print("d and s are equal")
+else:
+    print("s is greather than d")	
+print("programa finalizado")
